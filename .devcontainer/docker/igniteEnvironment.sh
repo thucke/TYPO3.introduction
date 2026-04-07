a0f216c8881e93005d63544d6bfa09072ffa0cd3
Thomas HUCKE <github@thucke.de>
2026-04-07 11:38:44 +0200

    feat: introduce TYPO3 Devcontainer template

---
+++ .devcontainer/docker/igniteEnvironment.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# 
+# !!!! IMPORTANT: WORKSPACE_ROOT has to be set before sourcing this script
+echo "BEGIN: igniteEnvironment.sh"
+
+set -u
+pushd ${WORKSPACE_ROOT}
+
+source .devcontainer/docker/parseDotEnv.sh
+
+echo "initializeTYPO3.sh: Reset environment"
+rm -rfv config .build/bin .build/public .build/vendor var
+mkdir -vp .build/public var/log/ var/lib/ $(dirname ${TYPO3_INSTALL_DB_DBNAME})
+
+if [ "${TYPO3_INSTALL_DB_DRIVER}" == "mysqli" ]; then
+  echo "Using MySQL/MariaDB as database - resetting database"
+  # Drop all database tables
+  mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} --silent --skip-column-names -e "SHOW TABLES" ${TYPO3_INSTALL_DB_DBNAME} | \
+      xargs -I% echo 'SET FOREIGN_KEY_CHECKS = 0; DROP TABLE %; SET FOREIGN_KEY_CHECKS = 1;' | \
+      mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} -v ${TYPO3_INSTALL_DB_DBNAME}
+
+    mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} --init-command='USE '${TYPO3_INSTALL_DB_DBNAME} < .devcontainer/docker/db/initdb/2_create_procedure.sql
+elif [ "${TYPO3_INSTALL_DB_DRIVER}" == "pdo_sqlite" ] && [ -f ${TYPO3_INSTALL_DB_DBNAME} ]; then
+  echo "Using SQLite as database - resetting database"
+  rm -fv ${TYPO3_INSTALL_DB_DBNAME}
+fi
+
+if [ -f ${WORKSPACE_ROOT}/composer.json ]; then
+  echo "initializeTYPO3.sh: Found composer.json - running initializeTYPO3.sh"
+  chmod +x .devcontainer/docker/initializeTYPO3.sh
+  .devcontainer/docker/initializeTYPO3.sh
+
+  echo "initializeTYPO3.sh: Running composer up"
+  composer up
+
+  if [ ! -f config/system/additional.php ]; then
+    echo "initializeTYPO3.sh: Add additional.php if not exists"
+    cp .devcontainer/docker/typo3/additional.php config/system/additional.php
+  else
+    echo "initializeTYPO3.sh: additional.php already exists - skipping"
+  fi
+
+  if [ "${TYPO3_INSTALL_DB_DRIVER}" == "mysqli" ]; then
+    echo "initializeTYPO3.sh: Fix TYPO3 image references"
+    mysql -h127.0.0.1 -P3306 -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} -e 'use '${TYPO3_INSTALL_DB_DBNAME}'; call fixImgInTtContent();'
+  else
+    echo "initializeTYPO3.sh: Fix TYPO3 image references - skipping as database driver is not mysqli"
+  fi
+
+  if [ "${COMPOSE_PROFILES}" == "php-fpm" ]; then
+    echo "initializeTYPO3.sh: Checking for running php-fpm"
+    .devcontainer/php-fpm/server.sh restart
+  elif [ "${COMPOSE_PROFILES}" == "apache" ]; then
+    echo "initializeTYPO3.sh: Restarting Apache"
+    .devcontainer/php-fpm/server.sh
+  elif [ "${COMPOSE_PROFILES}" == "frankenphp" ]; then
+    echo "initializeTYPO3.sh: Restarting FrankenPHP"
+    .devcontainer/docker/frankenphp/server.sh restart
+  fi
+
+else
+  echo "No composer.json found - skipping initializeTYPO3.sh and composer up"
+fi
+
+echo "END: igniteEnvironment.sh"
+popd
---
+++ .devcontainer/docker/parseDotEnv.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# parse .env file of the current project
+# the convention is that this file should reside in the parent dir of the project
+# having the same name as the project dir extended by '.env'
+# 
+# !!!! IMPORTANT: WORKSPACE_ROOT has to be set before sourcing this script
+set +u
+# quit if env vars have been already set
+[ -n "${DB_CONNECTION_ROOT_PASSWORD}" ] && echo ".env alread set" && return 0
+set -u
+
+dotEnvFile="$(dirname $WORKSPACE_ROOT)/TYPO3.env"
+
+# check if env file exists - if not copy default from .devcontainer
+if [ ! -f ${dotEnvFile} ]; then
+	echo "${dotEnvFile} file not found - copying default from DEV"
+	cp -fv ${WORKSPACE_ROOT}/.devcontainer/docker/typo3/TYPO3.env.tmpl ${dotEnvFile}
+fi
+
+# parse .env file
+if [ -f $dotEnvFile ]; then
+	echo "Parsing .env file $dotEnvFile"
+	export $(cat $dotEnvFile | grep -v -E "^\s*#.*$|^\s*$|#.*$" | xargs)
+else
+	echo "Skipping .env file $dotEnvFile"
+fi
+
+if [[ ${DB_SERVER_TYPE} =~ (mysql|mariadb) ]]; then
+  export TYPO3_INSTALL_DB_DRIVER="mysqli"
+  export TYPO3_INSTALL_DB_DBNAME="${MYSQLI_DBNAME}"
+elif [ "${DB_SERVER_TYPE}" == "sqlite" ]; then
+  export TYPO3_INSTALL_DB_DRIVER="pdo_sqlite"
+  export TYPO3_INSTALL_DB_DBNAME="${SQLITE_DBFILE_PATH}"
+fi
+
