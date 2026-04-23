ae21b27ce9715a338f3fca184594a680c5fa0355
Thomas Hucke <github@thucke.de>
2026-04-23 23:03:07 +0200

    chore: maybe found the issue

--- .devcontainer/docker/parseDotEnv.sh
+++ .devcontainer/docker/parseDotEnv.sh
@@ -11,7 +11,6 @@ set -u
 
 dotEnvFile="${WORKSPACE_ROOT}/.devcontainer/docker/typo3/TYPO3.env"
 
-set -x
 if [[ ${DB_SERVER_TYPE} =~ (mysql|mariadb) ]]; then
   export TYPO3_INSTALL_DB_DRIVER=mysqli
   export TYPO3_INSTALL_DB_DBNAME="${DB_SERVER_DBNAME}"
@@ -26,7 +25,6 @@ elif [ "${DB_SERVER_TYPE}" == "postgresql" ]; then
   export TYPO3_INSTALL_DB_DBNAME="${DB_SERVER_DBNAME}"
   export DB_SERVER_PORT=${DB_SERVER_PORT_POSTGRESQL}
 fi
-set +x
 
 if [[ ${COMPOSE_PROFILES} =~ "apache" ]]; then
   export TYPO3_INSTALL_WEB_SERVER_CONFIG=apache
