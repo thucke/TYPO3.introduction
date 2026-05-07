7fba69c4e611cfc87f7b9d9a4fc8e52838832013
Thomas Hucke <github@thucke.de>
2026-05-07 17:34:38 +0200

    chore: synch wich template

--- .devcontainer/docker/typo3/additional.php
+++ .devcontainer/docker/typo3/additional.php
@@ -10,8 +10,7 @@ if ($applicationContext->isDevelopment()) {
         [
             'SYS' => [
                 'exceptionalErrors' => getenv('TYPO3_CONFIG_SYS_EXCEPTIONALERRORS'),
-                'systemLocale' => 'en_US',
-                'trustedHostsPattern' => '127.0.0.1|localhost',
+                'systemLocale' => 'en_US'
             ],
         ]
     );
